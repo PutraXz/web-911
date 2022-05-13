b57cac0eada2597404686fdf693bef8a31fc9a58
putra <ytprem200100@gmail.com>
2022-05-13 17:22:53 +0700

    make it better

--- resources/views/components/jumbotron3.blade.php
+++ resources/views/components/jumbotron3.blade.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jumbotron-fluid" style="background: #362E2E;height:761px">
     <div class="container">
         <h1 class="display-5 text-white" style="width: 488px;font-weight:800;position: relative;top: 252px;">Jangkauan kami</h1>
-        <p class="lead text-white" style="width: 465px; height: 66px;position: relative;top: 301px;">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet</p>
+        <p class="lead text-white" style="width: 465px; height: 66px;position: relative;top: 301px;">lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet</p>
         <picture>
             <img src="{{ URL('images/image(20).png ') }}" alt="Card image cap" style="width: 147vh; left: 11rem;height: 77vh;" class="img-reponsive position-relative">
         </picture>
